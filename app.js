92253aa73d2d13929d47dd3ea7ed65a13a79abf5
yogeshdoko <yogesh.s@dokonaly.com>
2023-10-03 16:18:14 +0530

    Add cookie parser, fileupload and authorized middleware

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express()
 const path = require("path")
 const CONFIG = require("./config/config")
 const cors = require("cors")
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const { authorized } = require("./models/auth")
+
 require('dotenv').config()
 
 app.use(bodyParser.json())
@@ -13,9 +17,11 @@ app.use(cors({
     origin: "http://localhost:3000",
     credentials: true
 }))
+app.use(cookieParser())
+app.use(fileUpload())
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use("/fileuploads", express.static(path.join(__dirname, "/fileuploads"), { etag: false }))
+app.use("/fileuploads", authorized, express.static(path.join(__dirname, "/fileuploads"), { etag: false }))
 mongoose.connect(CONFIG.MONGOURL)
     .then(() => { console.log("mongodb is connected") })
     .catch((err => { console.log(err) }))
