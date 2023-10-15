f4529c01265326c2d2d86552d69072f058165949
yogesh200002 <yogeshvishal98@gmail.com>
2023-10-15 10:20:17 +0530

    Format code using prettier

--- schemas/disposalDetails.js
+++ schemas/disposalDetails.js
@@ -1,45 +1,48 @@
-const mongoose = require("mongoose")
-const ObjectId = mongoose.Schema.Types.ObjectId
-const disposalDetails = new mongoose.Schema({
-    whAckNo: {
-        type: String,
-        ref: "receipt",
-        require: true
-    },
-    eMalkhanaId: {
-        type: ObjectId,
-        ref: "eMalkhana",
-        require: true
-    },
-    eMalkhanaNo: {
-        type: String,
-        require: true
-    },
-    disposalMethod: {
-        type: String,
-        require: true
-    },
-    disposalAuctionOrder: {
-        type: String,
-        default: null
-    },
-    disposalType: {
-        type: String,
-        require: true
-    },
-    disposalRecovery:{
-        type:Array
-    },
-    reOpenUploadOrder: {
-        type: Array,
-        require: true
-    },
-    createdBy: {
-        type: ObjectId,
-        require: true,
-        ref: "user"
-    }
-},
-    { timestamps: true, versionKey: false }
-)
-module.exports = mongoose.model("disposal", disposalDetails)
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const disposalDetails = new mongoose.Schema(
+	{
+		whAckNo: {
+			type: String,
+			ref: "receipt",
+			require: true,
+		},
+		eMalkhanaId: {
+			type: ObjectId,
+			ref: "eMalkhana",
+			require: true,
+		},
+		eMalkhanaNo: {
+			type: String,
+			require: true,
+		},
+		disposalMethod: {
+			type: String,
+			require: true,
+		},
+		disposalAuctionOrder: {
+			type: String,
+			default: null,
+		},
+		disposalType: {
+			type: String,
+			require: true,
+		},
+		disposalRecovery: {
+			type: Array,
+		},
+		reOpenUploadOrder: {
+			type: Array,
+			require: true,
+		},
+		createdBy: {
+			type: ObjectId,
+			require: true,
+			ref: "user",
+		},
+	},
+	{ timestamps: true, versionKey: false }
+);
+
+module.exports = mongoose.model("disposal", disposalDetails);
